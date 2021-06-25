7588fa0c41773f539f91fd8275af1a5ce3b2ce29
JoaquimKayo <kayo_01@hotmail.com>
2021-06-25 20:16:54 -0300

    meu primeiro componente

---
+++ src/componentes/teste.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Teste extends Component {
+    render() {
+        return (
+            <div>
+                <h1>JoaquimKayo APP</h1>
+                <img src="../../logo512.png"/>
+            </div>
+        );
+    }
+}
+
+export default Teste;       
